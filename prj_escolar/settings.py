746d1f8b5c23a7e235d92fb77490b87eeb8808aa
rafaelponte89 <rafael_ponte1b@hotmail.com>
2021-09-22 10:44:10 -0300

    tela inicial: imagens, frases

--- prj_escolar/settings.py
+++ prj_escolar/settings.py
@@ -13,9 +13,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -126,6 +129,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 
 # Configure Django App for Heroku.
 import django_heroku
